f0f8d07944a72e735f570de78c2b486817d8222f
n-keerthi-gayathri <keerthigayathrinissankararao40@gmail.com>
2024-10-06 23:52:16 +0530

    MongoDB Connection Setup Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 // Import required modules
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
+const mongoose = require('mongoose');
 // Create an instance of the Express app
 const app = express();
 const PORT = process.env.PORT || 5000; // Use port from environment or default to 5000
@@ -14,6 +15,14 @@ app.use(express.json()); // Parse incoming JSON requests
 app.get('/status', (req, res) => {
     res.json({ message: 'API is running' }); // Send JSON response
 });
+
+//connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URL).then(() => {
+    console.log("DataBase Connected...");
+}).catch((err) => {
+    console.log(err);
+})
 
 // Start the server
 app.listen(PORT, () => {
